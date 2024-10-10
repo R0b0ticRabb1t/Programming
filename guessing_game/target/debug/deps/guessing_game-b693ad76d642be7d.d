95bb8f40a550585ea24dc251cda78524e54813c6
R0b0ticRabb1t <anirudha.py@gmail.com>
2024-10-11 00:35:26 +0400

    i don't actually know whats changed here though oh yeah i made rust-analyzer work but thats all i think hmm.-=.

---
+++ guessing_game/target/debug/deps/guessing_game-b693ad76d642be7d.d
@@ -0,0 +1,5 @@
+C:\Users\ASUS\OneDrive\Documents\Programming\guessing_game\target\debug\deps\libguessing_game-b693ad76d642be7d.rmeta: src/main.rs
+
+C:\Users\ASUS\OneDrive\Documents\Programming\guessing_game\target\debug\deps\guessing_game-b693ad76d642be7d.d: src/main.rs
+
+src/main.rs:
